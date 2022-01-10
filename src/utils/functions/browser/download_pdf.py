d132fa05a815fd85aacae7b8688a431fbc05f51a
Marcio Gonçalves Vieira <marciogv@gmail.com>
2022-01-10 17:58:31 -0300

    Delete src directory

--- src/utils/functions/browser/download_pdf.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def download_pdf (caminho_url_pdf, arquivo_pc_pdf):
-    resposta = requests.get(caminho_url_pdf)
-    with open (arquivo_pc_pdf, 'wb') as novo_arquivo:
-        novo_arquivo.write(resposta.content)
-        print('Fez download!')
--- src/utils/functions/transform_data/folder_extrair_tabelas_pdf/add_nome_arquivo_csv.py
+++
@@ -1,6 +0,0 @@
-
-def add_nome_arquivo_csv(tabela_atual):
-    coluna = tabela_atual.columns[-1]
-    nome_arquivo_csv = coluna.replace(" ", "")
-    nome_arquivo_csv = f'{nome_arquivo_csv}.csv'
-    return nome_arquivo_csv
